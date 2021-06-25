a922e7e0d6ebc0a02cfa4d016b06c12a551cdf78
Maximilian Ernestus <maximilian@ernestus.de>
2021-06-25 16:10:49 +0200

    Add welcome message and help text.

--- BrummaFirmware.ino
+++ BrummaFirmware.ino
@@ -9,6 +9,9 @@ auto motor_interface = Motorinterface<num_motors>(enable_pin);
 
 void setup(void) {
   Serial.begin(115200);
+  while(!Serial);  // Wait until serial device is up
+  Serial.print("==== Welcome to Brumma! ====\n\n");
+  printHelp();
   Serial.setTimeout(1000*60*60); // 1h timeout
   
 }
@@ -35,10 +38,16 @@ void loop(void) {
     for(size_t i = 0; i < command.length(); i++) {
       motor_interface.set(i, command.charAt(i) != '0');
     }
+    Serial.println("Updated Motors");
   }
   
 }
 
 void printHelp() {
-  
+  Serial.println(
+    "Commands:\n"
+    "'h' or 'H'\tdisplay this help text\n"
+    "'p<number>'\tset the power of the motors between 0 and 100\n"
+    "0010010\tor any variation thereof enables/disables the motors. The lenght has to match the number of motors\n"
+    );
 }
